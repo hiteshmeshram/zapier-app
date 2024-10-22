2d74bd9f280c68d7dc2226a09bc6b31a264f8c88
hiteshmeshram <hiteshmeshramt@gmail.com>
2024-10-22 17:38:11 +0530

    fix user session if he is logged in then redirects to dashboard

--- frontend/client/app/page.tsx
+++ frontend/client/app/page.tsx
@@ -1,9 +1,32 @@
+'use client'
+
 import { Appbar } from "@/components/Appbar";
 import { Hero } from "@/components/Hero";
 import { HeroVideo } from "@/components/HeroVideo";
+import { BASE_URL } from "@/config";
+import axios from "axios";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(()=>{
+    async function init() {
+      const res = await axios.get(`${BASE_URL}/api/v1/user`,{
+          headers: {
+              Authorization: localStorage.getItem('token')
+          }
+      })
+
+      if (res.data.user) {
+          router.push('/dashboard')
+      }
+  }
+  init()
+  },[])
+
   return (
     <div className="bg-orange-50">
       <Appbar />
